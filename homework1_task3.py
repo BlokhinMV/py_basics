0cae211f9c3c0ca2bca1e5ff19dd0326bbdea546
Uftkhang <blokhin.mv@yandex.ru>
2019-10-29 03:42:23 +0500

    Оставил только верный вариант

--- homework1_task3.py
+++ homework1_task3.py
@@ -4,12 +4,3 @@ nn = int(n * 2)
 nnn = int(n * 3)
 
 print(f'Сумма чисел {n}+{nn}+{nnn} = {int(n) + nn + nnn}')
-
-
-# Альтернативный вариант, без конкантенации строк:
-n = int(input('Введите число: '))
-
-nn = 10 * n + n
-nnn = 100 * n + nn
-
-print(f'Сумма чисел {n}+{nn}+{nnn} = {n + nn + nnn}')
